3dee3ede7c09814920be57bbcdc7daa67c7e2dff
Sophie Tai <tai_sophie@hotmail.com>
2022-03-23 17:55:17 +0800

    added toast to make button interactive

--- app/src/main/java/com/example/newdice/MainActivity.kt
+++ app/src/main/java/com/example/newdice/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.newdice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
+
 }
