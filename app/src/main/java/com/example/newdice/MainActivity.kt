3944e951e662091cad7bce2fd5e2d4316b907025
Sophie Tai <tai_sophie@hotmail.com>
2022-03-24 13:57:49 +0800

    set which image to show according to dice roll result

--- app/src/main/java/com/example/newdice/MainActivity.kt
+++ app/src/main/java/com/example/newdice/MainActivity.kt
@@ -23,16 +23,24 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 //    create new dice with 6 sides and roll it
         val dice = Dice(6)
-        //    update screen with dice roll
+        val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView2)
-        diceImage.setImageResource(R.drawable.dice_2)
+        //    update screen with dice roll
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
 
             }
 }
 
 class Dice(val numSides: Int) {
     //   if numSides was a private val, it would only be accessible in the dice class
-    fun smollRoll(): Int {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
