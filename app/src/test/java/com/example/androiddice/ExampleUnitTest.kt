1f5c294cf4e9f4a3b918c6bde1b4016db495b3f6
Sophie Tai <tai_sophie@hotmail.com>
2022-03-25 09:57:59 +0800

    [UNIT TEST] created first unit test with assertTrue function

--- app/src/test/java/com/example/androiddice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/androiddice/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.androiddice
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test as Test
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = smallDice(4)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
-}
+    }
